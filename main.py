dfb4cba29670897e1644ed71f920d8a56d3ebdbf
ssoxong <ssoxong@naver.com>
2024-10-29 23:46:07 +0900

    feat: difficulty decomposition

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, HTTPException
 from openai import OpenAI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 import os
 from dotenv import load_dotenv
-import re
 import json
+
+
 app = FastAPI()
 load_dotenv()
 
@@ -20,9 +21,9 @@ class PronounceRule(str, Enum):
     기식음화 = "기식음화"
 
 class ClaudeRequest(BaseModel):
-    age: int
+    age: int = Field(default=11)
     rule: PronounceRule
-    count: int
+    count: int = Field(default=5)
 
 @app.post("/claude")
 async def generate_claude(request: ClaudeRequest):
@@ -58,6 +59,33 @@ async def generate_claude(request: ClaudeRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+@app.post("/difficulty")
+async def calc_difficulty(s: str):
+    b_grade={
+        'ㄱ':2, 'ㄴ':2, 'ㄹ':2, 'ㅁ':2, 'ㅇ':2,
+        'ㄷ':3, 'ㅂ':3, 'ㅅ':3, 'ㅈ':3,  'ㅎ':3, 'ㅆ':3,
+        'ㅊ':4, 'ㅋ':4, 'ㅌ':4, 'ㅍ':4, 'ㄲ':4,
+        'ㄵ':5, 'ㄶ':5, 
+        'ㄺ':6, 'ㄻ':6, 'ㄼ':6, 'ㅀ':6, 'ㅄ':6,
+        'ㄳ':7, 'ㄽ':7, 'ㄾ':7, 'ㄿ':7, 
+    }
+    m_grade={
+        'ㅏ':1,  'ㅓ':1, 'ㅗ':1, 'ㅜ':1, 'ㅡ':1, 'ㅣ':1,
+        'ㅐ':2, 'ㅔ':2,
+        'ㅑ':3, 'ㅕ':3, 'ㅛ':3, 
+        'ㅚ':4, 'ㅟ':4,
+        'ㅘ':5, 'ㅝ':5, 'ㅢ':5,
+        'ㅖ':6, 'ㅙ':6, 'ㅞ':6,
+        'ㅒ':7, 'ㅠ':7,
+    }
+
+    b_list, m_list = decomposition(s)
+    print(b_list, m_list)
+    b_grade_sum = sum(b_grade.get(b) for b in b_list)
+    m_grade_sum = sum(m_grade.get(m) for m in m_list)
+    return b_grade_sum + m_grade_sum
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -65,6 +93,37 @@ async def root():
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
-    
 
 # uvicorn main:app --reload
+
+def decomposition(korean_word: str):
+    # 초성 리스트. 00 ~ 18
+    CHOSUNG_LIST = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+    # 중성 리스트. 00 ~ 20
+    JUNGSUNG_LIST = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
+    # 종성 리스트. 00 ~ 27 + 1(1개 없음)
+    JONGSUNG_LIST = [' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+    r_lst = []
+    for w in list(korean_word.strip()):
+        ## 영어인 경우 구분해서 작성함. 
+        if '가'<=w<='힣':
+            ## 588개 마다 초성이 바뀜. 
+            ch1 = (ord(w) - ord('가'))//588
+            ## 중성은 총 28가지 종류
+            ch2 = ((ord(w) - ord('가')) - (588*ch1)) // 28
+            ch3 = (ord(w) - ord('가')) - (588*ch1) - 28*ch2
+            r_lst.append([CHOSUNG_LIST[ch1], JUNGSUNG_LIST[ch2], JONGSUNG_LIST[ch3]])
+        else:
+            r_lst.append([w])
+    print (r_lst)
+    b_list = []
+    m_list = []
+    strip_list = [[col for col in row if col.strip()] for row in r_lst]
+
+    for i in strip_list:
+        m_list.append(i[1])
+        if len(i) == 3:
+            b_list.append(i[2])
+    
+    return b_list, m_list
