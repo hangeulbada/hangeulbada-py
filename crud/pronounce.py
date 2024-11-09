a7e3770577a6f87713befd41db243ff23706dc38
So Hyeon <ssoxong@naver.com>
2024-11-09 19:26:15 +0900

    Revert "feat: requirements (#2)"
    This reverts commit 42c4788cae78608535d2bd66602c690bc869ee68.

--- crud/pronounce.py
+++ crud/pronounce.py
@@ -1,6 +1,5 @@
 from pecab import PeCab
 import crud
-import crud.difficulty
 pecab = PeCab()
 
 def analysis_pronounce_crud(text):
@@ -8,8 +7,7 @@ def analysis_pronounce_crud(text):
     return {
         "구개음화": analysis_gugaeumhwa(text, dec),
         "비음화": analysis_beumhwa(text, dec),
-        "유음화": analysis_yueumhwa(text, dec),
-        "연음화": analysis_yeoneumhwa(text, dec)
+        "유음화": analysis_yueumhwa(text, dec)
     }
 
 def analysis_gugaeumhwa(text, dec):
@@ -53,19 +51,11 @@ def analysis_yueumhwa(text, dec):
 
 def analysis_yeoneumhwa(text, dec):
     yeoneumhwa=[]
-    pos = pecab.pos(text)
 
-    # 모음(ㅇ)으로 시작되는 조사, 어미, 접미사인 경우 연음화
-    josa = ['JKS', 'JKC', 'JKG', 'JKO', 'JKB', 'JKV', 'JKQ', 'JX', 'JC', 'EP', 'EF', 'EC', 'ETN', 'ETM', 'XSN','XSV','XSA']
-    for i, (word, tag) in enumerate(pos):
-        wdec = crud.difficulty.decomposition(word)
-        if wdec[-1][0] != 'ㅇ': continue
-        if tag not in josa: continue
-        forward = pos[i-1][0]
-        fdec = crud.difficulty.decomposition(forward)
-        fdec = [col for col in fdec[-1] if col.strip()]
-        if len(fdec)!=3: continue
-        yeoneumhwa.append(forward+word)
-    # 받침 뒤에 ㅏ, ㅓ, ㅗ, ㅜ, ㅟ로 시작하는 실질 형태소가 오는 경우
+    for i, r in enumerate(dec):
+        r = [col for col in r if col.strip()]
+        if len(r)!=3: continue
+        if r[2]=='ㄹ' and dec[i+1][0]=='ㄹ':
+            yeoneumhwa.append(text[i:i+2])
 
     return yeoneumhwa
